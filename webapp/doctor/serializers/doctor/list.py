c225aa1d6e8acaed716757ff349ff7b9af01e1ae
Rajan66 <rajanmaharjan042@gmail.com>
2025-11-01 03:25:11 +0545

    fix: add user seriazlier

--- webapp/doctor/serializers/doctor/list.py
+++ webapp/doctor/serializers/doctor/list.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers  # noqa
 from doctor.models import Doctor
 from hospital.serializers.department import ListDepartmentSerializer
+from gatekeeper.serializers import RetrieveMeSerializer
 
 
 class ListDoctorSerializer(serializers.ModelSerializer):
     department = ListDepartmentSerializer(read_only=True)
+    user = RetrieveMeSerializer(read_only=True)
 
     class Meta:
         model = Doctor
