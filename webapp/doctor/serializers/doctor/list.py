8cd2ad35efa5f37430ea291246d177bc20eaaf50
cloco-biplav <chitrakar.biplav@cloco.net>
2025-11-01 01:35:05 +0545

    fix: doctor serializer

--- webapp/doctor/serializers/doctor/list.py
+++ webapp/doctor/serializers/doctor/list.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers  # noqa
 from doctor.models import Doctor
+from hospital.serializers.department import ListDepartmentSerializer
 
 
 class ListDoctorSerializer(serializers.ModelSerializer):
+    department = ListDepartmentSerializer(read_only=True)
+
     class Meta:
         model = Doctor
         fields = "__all__"
