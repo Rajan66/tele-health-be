ef00499153e97740ae21763aee94fc1ddbb65ebf
Rajan66 <rajanmaharjan042@gmail.com>
2025-10-31 22:44:31 +0545

    feat(hospital): add hospital list and retrieve api

--- webapp/hospital/serializers/hospital/retrieve.py
+++ webapp/hospital/serializers/hospital/retrieve.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from hospital.models import Hospital
 
 
-class ListHospitalSerializer(serializers.Serializer):
+class RetrieveHospitalSerializer(serializers.Serializer):
     class Meta:
         model = Hospital
         fields = "__all__"
